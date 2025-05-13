78beb41624efc631dcb6ccfda2253208cdd0cd72
MuhammedChowdhury <your-email@example.com>
2025-05-14 02:46:20 +1000

    esolved merge confilicts and finalized project structure

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from app import create_app
 
 app = create_app()
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask app is running!"
+>>>>>>> afc1647 (Initialized Git and finalized project structure)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+<<<<<<< HEAD
 =======
 from app import create_app
 
@@ -15,3 +26,5 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 >>>>>>> dc8cfbe92444c4d49f3be126b10a798f1295dd81
+=======
+>>>>>>> afc1647 (Initialized Git and finalized project structure)
