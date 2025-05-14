2a73309ff9a3dcf1e654b8ad973ded802e63c699
MuhammedChowdhury <your-email@example.com>
2025-05-14 21:48:06 +1000

    Resolved merge conflicts in run.py

--- run.py
+++ run.py
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-from app import create_app
-
-app = create_app()
-=======
 from flask import Flask
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Flask app is running!"
->>>>>>> afc1647 (Initialized Git and finalized project structure)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-<<<<<<< HEAD
-=======
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
->>>>>>> dc8cfbe92444c4d49f3be126b10a798f1295dd81
-=======
->>>>>>> afc1647 (Initialized Git and finalized project structure)
